4ebbd449094c627ccb594939234eaaca35436b4b
Chris Stone <cstone@hubspot.com>
2021-12-06 15:16:21 -0800

    remove typo

--- 2021/day3/part1.js
+++ 2021/day3/part1.js
@@ -3,17 +3,17 @@ const inputsArray = inputsRaw
   .split('\n')
   .map(input => input.split('').map(Number));
 
-let starterArray = [0, 0, 0, 0, 0, 0 , 0, 0, 0, 0, 0, 0]
+let starterArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 // Perform counts, modify starter array
 inputsArray.map(input => {
   input.map((digit, i) => {
     digit ? starterArray[i]++ : starterArray[i];
   })
-})
+});
 
 const gamma = starterArray.map(count => count > 500 ? 1 : 0).join('');
 const epsilon = starterArray.map(count => count < 500 ? 1 : 0).join('');
 const solution = parseInt(gamma, 2) * parseInt(epsilon, 2);
-console.log(solution);
 
+console.log(solution);
