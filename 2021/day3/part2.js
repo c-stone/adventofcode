4471125b1e7d133a345a6d1718266d8390be9cd6
Chris Stone <cstone@hubspot.com>
2021-12-06 19:13:58 -0800

    day2 part2 solution

--- 2021/day3/part2.js
+++ 2021/day3/part2.js
@@ -1,19 +1,33 @@
 const { inputsRaw } = require ('./data/input');
-const inputsArray = inputsRaw
-  .split('\n')
-  .map(input => input.split('').map(Number));
 
-let starterArray = [0, 0, 0, 0, 0, 0 , 0, 0, 0, 0, 0, 0]
+const inputsArray = inputsRaw.split('\n');
+
+const countArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 // Perform counts, modify starter array
-inputsArray.map(input => {
-  input.map((digit, i) => {
-    digit ? starterArray[i]++ : starterArray[i];
-  })
-})
+const oneOrZeroByPosition = (position, inputsArray, countArray) => {
+  let starterArray = countArray;
+  inputsArray.map(input => {
+    input.map((digit, i) => {
+      digit ? starterArray[i]++ : starterArray[i];
+    })
+  });
 
-const gamma = starterArray.map(count => count > 500 ? 1 : 0).join('');
-const epsilon = starterArray.map(count => count < 500 ? 1 : 0).join('');
-const solution = parseInt(gamma, 2) * parseInt(epsilon, 2);
-console.log(solution);
+  return starterArray[position] <= inputsArray.length/2 ? 1 : 0
+};
 
+console.log(
+  inputsArray
+    .filter(input => input[0] == oneOrZeroByPosition(0, inputsArray, countArray))
+    .filter(input => input[1] == oneOrZeroByPosition(1, inputsArray, countArray))
+    .filter(input => input[2] == oneOrZeroByPosition(2, inputsArray, countArray))
+    .filter(input => input[3] == oneOrZeroByPosition(3, inputsArray, countArray))
+    .filter(input => input[4] == oneOrZeroByPosition(4, inputsArray, countArray))
+    .filter(input => input[5] == oneOrZeroByPosition(5, inputsArray, countArray))
+    .filter(input => input[6] == oneOrZeroByPosition(6, inputsArray, countArray))
+    .filter(input => input[7] == oneOrZeroByPosition(7, inputsArray, countArray))
+    .filter(input => input[8] == oneOrZeroByPosition(8, inputsArray, countArray))
+);
+
+// O2GeneratorRating = 011110000011 (4089)
+// cO2ScrubberRating = 111111111001 (1923)
