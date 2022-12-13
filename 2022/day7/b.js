8f67565f107fc80eea0160dff2957cbb8bde7e9f
Christopher Stone <cstone@hubspot.com>
2022-12-13 11:53:08 -0800

    day7 part2 solution

---
+++ 2022/day7/b.js
@@ -0,0 +1,55 @@
+const data = require("fs").readFileSync("input.txt", { encoding: "utf-8" }).trim();
+const input = data.split('\n');
+
+const fs = { files: {}, dirs: {} };
+
+let current = fs;
+
+for (const command of input) {
+  const [cmd, ...rest] = command.split(" ");
+
+  if (cmd === "$") {
+    if (rest[0] === "cd") {
+      const dir = rest[1];
+      if (dir === "..") {
+        current = current.parent;
+      } else if (dir === "/") {
+        current = fs;
+      } else {
+        if (!current.dirs[dir]) {
+          current.dirs[dir] = { parent: current, files: {}, dirs: {} };
+        }
+        current = current.dirs[dir];
+      }
+    }
+  } else if (cmd !== "dir") {
+    current.files[rest[0]] = parseInt(cmd);
+  }
+}
+
+const dirSizes = [];
+
+const computeFileSize = (curr) => {
+  let size = 0;
+
+  for (const file in curr.files) {
+    size += curr.files[file];
+  }
+
+  for (const dir in curr.dirs) {
+    const dirSize = computeFileSize(curr.dirs[dir]);
+    size += dirSize;
+
+    dirSizes.push(dirSize);
+  }
+
+  return size;
+};
+
+const rootSize = computeFileSize(fs);
+
+const smallestThatFixesSpace = Math.min(
+  ...dirSizes.filter((x) => x >= 30000000 - (70000000 - rootSize))
+);
+
+console.log(smallestThatFixesSpace);
