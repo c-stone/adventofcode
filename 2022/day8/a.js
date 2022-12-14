18171a4f0489bab1db142b8713aa330ea3096ea1
Christopher Stone <cstone@hubspot.com>
2022-12-13 20:18:04 -0800

    solution day 1

--- 2022/day8/a.js
+++ 2022/day8/a.js
@@ -1,36 +1,40 @@
-const data = require("fs").readFileSync("input.txt", { encoding: "utf-8" }).trim().split('\n');
+const rows = require("fs").readFileSync("input.txt", { encoding: "utf-8" }).trim().split('\n');
 
-const width = data[0].length;
-const height = data.length;
+const width = rows[0].length;
+const height = rows.length;
 
 let visibleCount = 0;
 
-data.forEach((row, rowIndex) => {
+rows.forEach((row, rowIndex) => {
   row = row.split('');
 
   row.forEach((tree, columnIndex) => {
     const isFirstOrLastRow = rowIndex === 0 || rowIndex === height-1;
-    const isFirstOrLastTree = !isFirstOrLastRow && columnIndex === 0 || columnIndex === width-1;
-    const currentRow = row;
-    const currentColumn = data.map(x => x[columnIndex]);
-
-    // Remove current tree
-    currentRow.splice(columnIndex, 1);
-    currentColumn.splice(rowIndex, 1);
-
-    const treesToCompare = currentRow.concat(currentColumn);
-    const rowInvisible = treesToCompare.some(x => x > tree);
+    const isFirstOrLastTreeInRow = !isFirstOrLastRow && columnIndex === 0 || columnIndex === width-1;
     
-    if (isFirstOrLastRow || isFirstOrLastTree) {
+    const currentRow = [...row];
+    const currentColumn = [...rows].map(x => x[columnIndex]);
+
+    const above = currentColumn.slice(0, rowIndex);
+    const right = currentRow.slice(columnIndex+1, width);
+    const below = currentColumn.slice(rowIndex+1, height);
+    const left = currentRow.slice(0, columnIndex);
+
+    const treesToCompare = [above, right, below, left];
+    const treeVisibilityByDirection = treesToCompare.map(direction => direction.some(otherTree => otherTree >= tree));
+    const isTreeVisible = treeVisibilityByDirection.some(invisible => !invisible);
+g
+    if (isFirstOrLastRow || isFirstOrLastTreeInRow) {
       visibleCount++;
       return;
     }
 
-    if (!rowInvisible) {
+    if (isTreeVisible) {
       visibleCount++;
       return;
     }
   });
 });
 
+// Solution
 console.log(visibleCount);
